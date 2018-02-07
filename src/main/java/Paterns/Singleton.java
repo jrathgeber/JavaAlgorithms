6e3ecaae675bd0cb97783cdca9634e0303763173
Jason Rathgeber <jrathgeber@yahoo.com>
2018-02-07 15:53:41 -0500

    Add files via upload

---
+++ src/main/java/Paterns/Singleton.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paterns;
+
+/**
+ *
+ * @author jrathgeber
+ */
+public class Singleton {
+    
+    private static Singleton instance = null;
+        
+    private Singleton () {} ;
+    
+    public static Singleton getSingleton() {
+                
+        if (null == instance) {
+
+            synchronized (Singleton.class){
+       
+                if (instance == null) {
+
+                    instance = new Singleton();
+                    System.out.println("Creating new instance");
+                }
+            }
+       } 
+            
+            
+        return instance;
+     
+    }    
+        
+     public void printSingleton(){
+
+        System.out.println("Inside print Singleton");
+    }
+        
+     
+    public static void main (String... aArguments) {
+        
+        Singleton sg = Singleton.getSingleton() ;
+        
+        //sg.printSingleton();
+        
+        Singleton sx = Singleton.getSingleton()  ;
+        
+        Singleton sz = new Singleton ();
+        
+        sz.printSingleton();
+    }
+                       
+}
