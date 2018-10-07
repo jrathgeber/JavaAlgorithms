d28b19d077029cebc3b0904e793c6fd2d23dc7ff
jrathgeber <jrathgeber@yahoo.com>
2018-10-07 11:23:00 -0400

    Last for today maybe

--- src/main/java/Algorithms/Misc/BracketChecker.java
+++ src/main/java/Algorithms/Misc/BracketChecker.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Algorithms.BinarySearch;
+package Algorithms.Misc;
 
 import java.io.IOException;
 
--- src/main/java/Algorithms/Misc/Roman.java
+++ src/main/java/Algorithms/Misc/Roman.java
@@ -15,86 +15,46 @@ import java.nio.charset.StandardCharsets;
  * @author Jason
  */
 public class Roman {
-            
+
+
+    public String intToRoman(int num) {
+
+        Integer i = num;
+
+        String ans = "";
+
+        String[] m = {"", "M", "MM", "MMM"};
+        String[] c = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] x = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] l = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        int mod10 = i % 10;
+        int mod100 = i % 100;
+        int mod1000 = i % 1000;
+
+        int div1000 = i / 1000;
+
+        ans += m[div1000];
+        ans += c[mod1000/100];
+        ans += x[mod100/10];
+        ans += l[mod10];
+
+        return ans;
+
+    }
+
+
+
     public static void main(String[] args) throws IOException {
-    
-    InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
-    
-    BufferedReader in = new BufferedReader(reader);
 
-    String line;
-        
-        while ((line = in.readLine()) != null) {
+        Roman r = new Roman();
 
-          Integer i = Integer.parseInt(line);
+        String roman = r.intToRoman(1972);
 
-          String ans = "X = "; 
-          
-          int mod1 =  i % 1;
-          int mod5 =  i % 5;
-          int mod10 = i % 10;
-          int mod50 = i % 50;
-          int mod100 = i % 100;
-          int mod500 = i % 500;
-          int mod1000 = i % 1000;
-                              
-          int div1 =  i / 1;
-          int div5 =  i / 5;
-          int div10 = i / 10;
-          int div50 = i / 50;
-          int div100 = i / 100;
-          int div500 = i / 500;
-          int div1000 = i / 1000;
-          
-          
-          if (div1000 > 1 ) {
-          
-            for (int x=0;x<div1000;x++){  
-                ans += "M";  
-            }
-              
-          } else if (div500 > 1) {
-                      
-            for (int x=0;x<div500;x++){  
-                ans += "D";  
-            }         
-                  
-          } else if (div100 > 1) {
-                      
-            for (int x=0;x<div100;x++){  
-                ans += "C";  
-            }         
-                  
-          
-          } else if (div50 > 1) {
-                      
-            for (int x=0;x<div50;x++){  
-                ans += "L";  
-            }         
-         
-          } else if (div10 > 1) {
-                      
-            for (int x=0;x<div10;x++){  
-                ans += "X";  
-            }         
-                     
-          } else if (div5 > 1) {
-                      
-            for (int x=0;x<div5;x++){  
-                ans += "V";  
-            }         
-                  
-          } else if (div1 < 4) {
-                      
-            for (int x=0;x<div1;x++){  
-                ans += "I";  
-            }    
-            
-          }                    
-                   
-          System.out.println(ans);
-         
-          }
-    }  
+        System.out.println(roman);
+
+    }
+
+
 
 }
