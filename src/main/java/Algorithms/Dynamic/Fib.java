bc2a97166f1f87ff85462276b84662c2b87bc186
Jason Rathgeber <jrathgeber@yahoo.com>
2018-11-30 16:24:26 -0500

    dynamic programming

---
+++ src/main/java/Algorithms/Dynamic/Fib.java
@@ -0,0 +1,21 @@
+package Algorithms.Dynamic;
+
+public class Fib {
+
+
+    static int fib(int n)
+    {
+        if (n <= 1)
+            return n;
+        return fib(n-1) + fib(n-2);
+    }
+
+
+    public static void main (String args[])
+    {
+        int n = 9;
+        System.out.println(fib(n));
+    }
+
+
+}
---
+++ src/main/java/Algorithms/Dynamic/Ugly.java
@@ -0,0 +1,4 @@
+package Algorithms.Dynamic;
+
+public class Ugly {
+}
