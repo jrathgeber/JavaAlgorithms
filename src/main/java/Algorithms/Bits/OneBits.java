be2521ff59c1fde2b67d3399875f8fb37f1a169e
jrathgeber <jrathgeber@yahoo.com>
2018-12-25 20:42:51 -0500

    bits2
    Signed-off-by: jrathgeber <jrathgeber@yahoo.com>

--- src/main/java/Algorithms/Bits/OneBits.java
+++ src/main/java/Algorithms/Bits/OneBits.java
@@ -1,5 +1,10 @@
 package Algorithms.Bits;
+/*
+    191. Number of 1 Bits
 
+    Write a function that takes an unsigned integer and return the number of '1' bits it has (also known as the Hamming weight).
+
+ */
 public class OneBits {
 
     public static void main(String... args) {
@@ -8,7 +13,7 @@ public class OneBits {
 
         int test1 = a.hammingWeight(00000000000000000000000000001011);
         int expected1 = 3;
-        System.out.println(" Out [" + test1 + "] expected + [" + test1 + "]" );
+        System.out.println(" Out [" + test1 + "] expected + [" + expected1 + "]" );
         assert test1 == expected1 : "Error";
     }
 
---
+++ src/main/java/Algorithms/Bits/OneTwoBits.java
@@ -0,0 +1,48 @@
+package Algorithms.Bits;
+/*
+    717. 1-bit and 2-bit Characters
+
+    We have two special characters.
+    The first character can be represented by one bit 0.
+    The second character can be represented by two bits (10 or 11).
+
+    Now given a string represented by several bits.
+    Return whether the last character must be a one-bit character or not.
+    The given string will always end with a zero.
+
+    Note:
+    1 <= len(bits) <= 1000.
+    bits[i] is always 0 or 1.
+
+ */
+public class OneTwoBits {
+
+    public static void main(String... args) {
+
+        OneTwoBits a = new OneTwoBits();
+
+        boolean test1 = a.isOneBitCharacter(new int[]{1, 0, 0});
+        boolean expected1 = true;
+        System.out.println(" Out [" + test1 + "] expected + [" + expected1 + "]" );
+        assert test1 == expected1 : "Error";
+
+        boolean test2 = a.isOneBitCharacter(new int[]{1, 1, 1, 0});
+        boolean expected2 = false;
+        System.out.println(" Out [" + test2 + "] expected + [" + expected2 + "]" );
+        assert test1 == expected1 : "Error";
+
+    }
+
+
+
+    // you need to treat n as an unsigned value
+    public boolean isOneBitCharacter(int[] bits) {
+
+
+        return true;
+
+    }
+
+
+
+}
