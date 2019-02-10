d4b7d370c4e6e3709b5cff18cf6aee1d6cb72378
jrathgeber <jrathgeber@yahoo.com>
2019-02-10 16:30:06 -0500

    MyQueue
    Signed-off-by: jrathgeber <jrathgeber@yahoo.com>

--- src/main/java/Structures/MyQueue.java
+++ src/main/java/Structures/MyQueue.java
@@ -1,4 +1,95 @@
 package Structures;
 
-public class MyQueue {
+import java.util.NoSuchElementException;
+
+
+/*
+
+    My Queue for CTJJI
+
+    0. Make a node
+    1. first, last pointers
+    2. add and rotate
+    3. remove
+    4. peek
+
+ */
+
+
+public class MyQueue<T> {
+
+    // Object for node
+    private static class QueueNode<T> {
+
+        private T data;
+        private QueueNode<T> next;
+
+        public QueueNode(T data) {
+            this.data = data;
+        }
+
+    }
+
+    // Pointers First and Last
+    private QueueNode<T> first;
+    private QueueNode<T> last;
+
+    // Add an elememt to queue
+    public void add(T item) {
+
+        QueueNode<T> t = new QueueNode<T>(item);
+
+        // if has an element
+        if(last != null){
+            last.next = t;
+        }
+        // set the element
+        last = t;
+        // set first in queue
+        if(first == null) {
+            first = last;
+        }
+
+    }
+
+    // Remove Element
+    public T remove() {
+
+        if (first == null) throw new NoSuchElementException();
+
+        T data = first.data;
+        first = first.next;
+        if (first == null) {
+            last = null;
+        }
+
+        return data;
+
+    }
+
+    // Get really
+    public T peek() {
+
+        if (first == null) throw new NoSuchElementException();
+        return first.data;
+
+    }
+
+    // no first its empty
+    public boolean isEmpty() {
+         return first == null;
+    }
+
+
+
+    // Main
+    public static void main (String ... args) {
+
+        MyQueue<String> qt = new MyQueue<String>();
+
+        qt.add("Hello");
+
+    }
+
+
 }
