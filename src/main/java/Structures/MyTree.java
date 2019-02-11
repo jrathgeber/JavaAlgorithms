cb806d3a828b222e33a1fdd3d4ecfb08a01cd891
Jason Rathgeber <jrathgeber@yahoo.com>
2019-02-11 17:53:34 -0500

    tree

--- src/main/java/Structures/MyTree.java
+++ src/main/java/Structures/MyTree.java
@@ -1,4 +1,157 @@
 package Structures;
 
+/*
+
+    Simple Tree Class albeit with a bit of recursion to make things interesting !
+    From : https://www.baeldung.com/java-binary-tree
+
+ */
+
+
+import java.util.*;
+
+// The node
+// Left Right Value
+class Node {
+
+    int value;
+    Node left;
+    Node right;
+
+    Node(int value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+
+}
+
+
+
 public class MyTree {
+
+    Node root;
+
+    // Base Add
+    public void add(int value) {
+        root = addRecursive(root, value);
+    }
+
+    private Node addRecursive(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+
+        if (value < current.value) {
+            current.left = addRecursive(current.left, value);
+        } else if (value > current.value) {
+            current.right = addRecursive(current.right, value);
+        } else {
+            // value already exists
+            return current;
+        }
+
+        return current;
+    }
+
+    private boolean containsNodeRecursive(Node current, int value) {
+        if (current == null) {
+            return false;
+        }
+        if (value == current.value) {
+            return true;
+        }
+        return value < current.value
+                ? containsNodeRecursive(current.left, value)
+                : containsNodeRecursive(current.right, value);
+    }
+
+
+    private Node deleteRecursive(Node current, int value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (value == current.value) {
+            // Node to delete found
+            // ... code to delete the node will go here
+        }
+        if (value < current.value) {
+            current.left = deleteRecursive(current.left, value);
+            return current;
+        }
+        current.right = deleteRecursive(current.right, value);
+        return current;
+    }
+
+    // 4. Traversing the tree
+
+    // Depth first
+    public void traverseInOrder(Node node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.value);
+            traverseInOrder(node.right);
+        }
+    }
+
+
+    public void traversePreOrder(Node node) {
+        if (node != null) {
+            System.out.print(" " + node.value);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+
+    public void traversePostOrder(Node node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.value);
+        }
+    }
+
+
+    //4.2 Breadth - First Search
+    public void traverseLevelOrder() {
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> nodes = new LinkedList<>();
+        nodes.add(root);
+
+        while (!nodes.isEmpty()) {
+
+            Node node = nodes.remove();
+
+            System.out.print(" " + node.value);
+
+            if (node.left != null) {
+                nodes.add(node.left);
+            }
+
+            if (node.right!= null) {
+                nodes.add(node.right);
+            }
+        }
+    }
+
+
+    // Test Starter
+    public static void main (String...args) {
+
+        MyTree tree = new MyTree();
+        tree.add(1);
+        tree.add(2);
+
+        boolean bt = tree.containsNodeRecursive( tree.root, 2);
+
+        System.out.println("Test [" + bt +  "]");
+
+
+    }
+
 }
