f8b40d60566a81e3372da4e0dcd072defbb8ec80
jrathgeber <jrathgeber@yahoo.com>
2019-03-14 05:50:42 -0400

    VOL
    Signed-off-by: jrathgeber <jrathgeber@yahoo.com>

--- src/main/java/Threads/VolatileTest.java
+++ src/main/java/Threads/VolatileTest.java
@@ -24,8 +24,7 @@ public class VolatileTest {
             int local_value = MY_INT;
             while ( local_value < 5){
                 if( local_value!= MY_INT){
-                    //LOGGER.log(Level.INFO,"Got Change for MY_INT : {0}", MY_INT);
-                    System.out.println("Got Change for MY_INT : {0}" +  MY_INT);
+                    System.out.println("Got Change for MY_INT : [" +  MY_INT + "]");
                     local_value= MY_INT;
                 }
             }
@@ -38,8 +37,7 @@ public class VolatileTest {
 
             int local_value = MY_INT;
             while (MY_INT <5){
-                //LOGGER.log(Level.INFO, "Incrementing MY_INT to {0}", local_value+1);
-                System.out.println("Incrementing MY_INT to {0} " + local_value+1);
+                System.out.println("Incrementing MY_INT to  [" + local_value + 1 + "]");
                 MY_INT = ++local_value;
                 try {
                     Thread.sleep(500);
