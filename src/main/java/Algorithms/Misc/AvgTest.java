899fd1257e436690ce3336fe2d10fc6e22fcd027
jrathgeber <jrathgeber@yahoo.com>
2019-12-07 12:36:08 -0500

    look
    Signed-off-by: jrathgeber <jrathgeber@yahoo.com>

---
+++ src/main/java/Algorithms/Misc/AvgTest.java
@@ -0,0 +1,24 @@
+package Algorithms.Misc;
+
+public class AvgTest {
+
+
+
+
+    public static void main (String ... args ) {
+
+        double averge_test =  (7 + 4) / 2 ;
+
+        System.out.println("Average Test is [" + averge_test + "]");
+
+
+
+        double averge_test2 =  (7.0 + 4.0) / 2.0 ;
+
+        System.out.println("Average Test2 is [" + averge_test2 + "]");
+
+
+
+    }
+
+}
