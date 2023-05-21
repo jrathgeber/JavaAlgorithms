5b7be0a4b1e631deb3344a5c7d0674781bf47e8f
jrathgeber <jrathgeber@yahoo.com>
2023-05-21 10:10:44 -0400

    look
    Signed-off-by: jrathgeber <jrathgeber@yahoo.com>

--- src/main/java/Advent/day1/day1.java
+++ src/main/java/Advent/day1/day1.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.IOException;
 
+/* May 21st 2023. This is all updated for JDK 20 */
+
+
 public class day1
 {
     public static void main(String[] args) throws IOException
