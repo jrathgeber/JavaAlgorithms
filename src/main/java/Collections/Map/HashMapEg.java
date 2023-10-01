c4c1ef261065146a26ed2bd6cf3d817fa5bd5560
jrathgeber <jrathgeber@yahoo.com>
2023-10-01 18:09:14 -0400

    mappy
    Signed-off-by: jrathgeber <jrathgeber@yahoo.com>

--- src/main/java/Collections/Map/HashMapEg.java
+++ src/main/java/Collections/Map/HashMapEg.java
@@ -1,21 +1,22 @@
 package Collections.Map;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapEg {
-
 
 
     public static void main (String...args) {
 
         HashMap<String,String> test = new HashMap <String,String> ();
 
-
         test.put ("hi", "ho");
+        test.put ("we", "got");
 
-
+        for (Map.Entry<String,String> entry: test.entrySet()) {
+            System.out.println("Key = " + entry.getKey());
+        }
 
     }
-
 
 }
