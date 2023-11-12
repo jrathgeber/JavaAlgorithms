d5ef6857858c88f6a448300229c72365b69f4424
jrathgeber <jrathgeber@yahoo.com>
2023-11-11 21:40:53 -0500

    map
    Signed-off-by: jrathgeber <jrathgeber@yahoo.com>

--- src/main/java/Collections/Map/HashMapEg.java
+++ src/main/java/Collections/Map/HashMapEg.java
@@ -8,14 +8,35 @@ public class HashMapEg {
 
     public static void main (String...args) {
 
-        HashMap<String,String> test = new HashMap <String,String> ();
+        HashMap<String,String> capitalCities = new HashMap <String,String> ();
 
-        test.put ("hi", "ho");
-        test.put ("we", "got");
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
 
-        for (Map.Entry<String,String> entry: test.entrySet()) {
+
+        for (Map.Entry<String,String> entry: capitalCities.entrySet()) {
             System.out.println("Key = " + entry.getKey());
         }
+
+        String x = capitalCities.get("USA");
+
+        System.out.println(x);
+
+        // Better
+        for (String i : capitalCities.keySet()) {
+            System.out.println("Key : " + i);
+        }
+
+
+        // Print values
+        for (String i : capitalCities.values()) {
+            System.out.println("Value : " + i);
+        }
+
+
+
 
     }
 
