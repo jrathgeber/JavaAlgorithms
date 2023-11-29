83e2c9d64171592f36027027624559b3afbdcf34
jrathgeber <jrathgeber@yahoo.com>
2023-11-28 21:21:26 -0500

    Permcheck1
    Signed-off-by: jrathgeber <jrathgeber@yahoo.com>

---
+++ src/main/java/Algorithms/Codility/PermCheck.java
@@ -0,0 +1,96 @@
+package Algorithms.Codility;
+
+/*
+
+A non-empty array A consisting of N integers is given.
+
+A permutation is a sequence containing each element from 1 to N once, and only once.
+
+For example, array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+is a permutation, but array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+is not a permutation, because value 2 is missing.
+
+The goal is to check whether array A is a permutation.
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A, returns 1 if array A is a permutation and 0 if it is not.
+
+For example, given array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+the function should return 1.
+
+Given array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+the function should return 0.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [1..1,000,000,000].
+
+ */
+
+
+public class PermCheck {
+
+
+    public static void main(String[] args) {
+
+        zTemplate sl = new zTemplate();
+        int test1 = sl.solution(1);
+        int test2 = sl.solution(1);
+        int test3 = sl.solution(1);
+
+        int dave1[] = sl.solution(new int[]{2,3,1,5});
+        int dave2[] = sl.solution(new int[]{1,2,4,5,6});
+        int dave3 []= sl.solution(new int[]{1,2,4,5,6});
+
+        int expected1 = 5;
+        System.out.println(" Out1 [" + test1 + "] expected + [" + expected1 + "] dave + [" + dave1 + "]" );
+        assert test1 == expected1 : "Error";
+
+        int expected2 = 0;
+        System.out.println(" Out2 [" + test2 + "] expected + [" + expected2 + "] dave + [" + dave2 + "]" );
+        assert test2 == expected2 : "Error";
+
+        int expected3 = 0;
+        System.out.println(" Out3 [" + test3 + "] expected + [" + expected3 + "] dave + [" + dave3 + "]" );
+        assert test3 == expected3 : "Error";
+
+    }
+
+    public int solution(int n) {
+
+        return n;
+
+    }
+
+
+    public int[] solution(int[] A) {
+
+        int a[] = {1,2,4,5,11};
+
+        return a;
+
+    }
+
+}
