83607092b1bd8ad2b3ae4de3103ca5e6a60dee35
jrathgeber <jrathgeber@yahoo.com>
2023-12-03 08:04:57 -0500

    passing cars
    Signed-off-by: jrathgeber <jrathgeber@yahoo.com>

--- src/main/java/Algorithms/Codility/PassingCars.java
+++ src/main/java/Algorithms/Codility/PassingCars.java
@@ -49,13 +49,13 @@ public class PassingCars {
     public static void main(String[] args) {
 
         PassingCars sl = new PassingCars();
-        int test1 = sl.solution(new int[]{0,1,0,1,1});
-        int test2 = sl.solution(new int[]{0,0,0,0});
-        int test3 = sl.solution(new int[]{1,0,1,1});
+        int test1 = sl.solution_1(new int[]{0,1,0,1,1});
+        int test2 = sl.solution_1(new int[]{0,0,0,0});
+        int test3 = sl.solution_1(new int[]{1,0,1,1});
 
-        int dave1 = sl.solution(new int[]{0,1,0,1,1});
-        int dave2 = sl.solution(new int[]{0,0,0,0,0});
-        int dave3 = sl.solution(new int[]{1,0,1,1});
+        int dave1 = sl.solution_2(new int[]{0,1,0,1,1});
+        int dave2 = sl.solution_2(new int[]{0,0,0,0,0});
+        int dave3 = sl.solution_2(new int[]{1,0,1,1});
 
         int expected1 = 5;
         System.out.println(" Out1 [" + test1 + "] expected + [" + expected1 + "] dave + [" + dave1 + "]" );
@@ -65,13 +65,13 @@ public class PassingCars {
         System.out.println(" Out2 [" + test2 + "] expected + [" + expected2 + "] dave + [" + dave2 + "]" );
         assert test2 == expected2 : "Error";
 
-        int expected3 = 0;
+        int expected3 = 2;
         System.out.println(" Out3 [" + test3 + "] expected + [" + expected3 + "] dave + [" + dave3 + "]" );
         assert test3 == expected3 : "Error";
 
     }
 
-    public int solution(int[] A) {
+    public int solution_1(int[] A) {
 
         int ans=0;
         int i = 0;
@@ -88,6 +88,24 @@ public class PassingCars {
             i++;
         }
         return ans;
+    }
+
+
+    public int solution_2(int[] A) {
+
+        int zeros = 0;
+        int passed = 0;
+
+        for (int a : A) {
+
+            if (a==0) {
+                zeros ++;
+            } else {
+                passed = passed + (1 * zeros);
+                System.out.println(zeros + " " +  passed);
+            }
+        }
+        return passed;
     }
 
 }
